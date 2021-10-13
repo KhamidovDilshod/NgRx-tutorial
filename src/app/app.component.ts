b0772eeb906e896a7eb05657addba00c8b4bfc55
dostonbekkhamidov@gmail.com <doston1994iyul23>
2021-10-13 12:09:00 +0500

    NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {isArrayEqual} from "@angular/compiler-cli/src/ngtsc/incremental/semantic_graph";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NgRX';
+  calculate: string | undefined;
+  result: any;
+  small: any;
+  big: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+find(){
+    if (arr[])
+}
+
 }
